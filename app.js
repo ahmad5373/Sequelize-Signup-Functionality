3c0692e53995eea4759cee5412981bf0c9d2827a
ahmed raza <ahmedraxa550@gmail.com>
2022-05-19 18:15:47 +0500

    Intial commit of source code to repo

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const user = require("./routes/user");
+const database = require("./database/connection");
+const PORT = process.env.PORT || 1500;
+
+app.get("/", (req, res) => {
+  res.send("Hello World This is Test Case");
+});
+
+//express.json Use as a middleware
+app.use(express.json());
+
+//Using Express middleware in this main file
+app.use("/user", user);
+
+app.listen(PORT, () => {
+  console.log(`app is listening on : http//localhost:${PORT}`);
+});
